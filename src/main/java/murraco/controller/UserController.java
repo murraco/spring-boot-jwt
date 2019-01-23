fc19d48a32fd241a11778eeeef922e150c3de0b1
Hani AlMutairi <haniswe5@gmail.com>
2019-01-23 11:57:33 +0300

    Refresh Tokens

--- src/main/java/murraco/controller/UserController.java
+++ src/main/java/murraco/controller/UserController.java
@@ -93,4 +93,10 @@ public class UserController {
     return modelMapper.map(userService.whoami(req), UserResponseDTO.class);
   }
 
+  @GetMapping("/refresh")
+  @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_CLIENT')")
+  public String refresh(HttpServletRequest req) {
+    return userService.refresh(req.getRemoteUser());
+  }
+
 }
